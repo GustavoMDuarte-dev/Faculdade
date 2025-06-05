abae516294c97463b90b154d48ffd0a8bb340f90
Gustavo Meneses Duarte <gustavomduarte8@gmail.com>
2025-06-05 15:54:31 -0300

    Remove pasta webII

--- webII/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-// CONFIGURE AQUI COM SEUS DADOS
-const sequelize = new Sequelize('loja_cestas_db', 'postgres', 'postgres', {
-    host: 'localhost',
-    dialect: 'postgres'
-});
-
-module.exports = sequelize;
